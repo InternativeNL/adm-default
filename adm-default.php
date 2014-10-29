6778f958424d343fe499dc1fe10252e716e4845d
Erik de Vries <erik@admium.nl>
2014-10-29 14:48:35 +0100

    Initial commit

---
+++ adm-default.php
@@ -0,0 +1,138 @@
+<?php
+/*
+Plugin Name: Admium default options
+Plugin URI: www.admium.nl
+Description: Several default options for Admium Wordpress CMS.
+Author: Admium
+Version: 0.1
+Author URI: www.admium.nl
+GitHub Plugin URI: AdmiumNL/adm-default
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+
+function remove_wordpress_generator() {
+	return '';
+}
+add_filter('the_generator', 'remove_wordpress_generator', 1);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Style the login page to Admium design
+
+function add_custom_login_image() {
+    echo '
+    <style type="text/css">
+        #login {
+        	width: 264px !important;
+        }
+        #loginform, #lostpasswordform {
+            border-radius: 5px;
+            -webkit-border-radius: 5px;
+    		-webkit-box-shadow: 0px 1px 2px 0 rgba(0,0,0,0.3);
+    		box-shadow: 0px 1px 2px 0 rgba(0,0,0,0.3);
+    		background: #ffffff; /* Old browsers */
+    		-webkit-appearance: none;
+        }
+        #loginform #wp-submit {
+            margin-top:12px;
+        }
+        #wp-submit {
+            width:100%;
+        }
+        #nav a, #backtoblog a {
+            color: #ffffff !important;
+        }
+        .login #backtoblog, .login #nav {
+            text-align:center !important;
+        }
+        h1 a { 
+            background-image:url(http://www.admium.nl/plugin/admium_logo.png) !important;
+            background-size: 264px 85px !important;
+            height: 85px !important;
+            width: 264px !important;
+        }
+        body {
+            background:none !important;
+        }
+        html {
+            background: url(http://www.admium.nl/plugin/background.jpg) no-repeat center center fixed !important;
+            -webkit-background-size: cover !important;
+            -moz-background-size: cover !important;
+            -o-background-size: cover !important;
+            background-size: cover !important;
+        }
+        .wp-core-ui .button-primary {
+            background: #228fcf !important;
+        }
+    </style>
+    ';
+}
+add_action('login_head', 'add_custom_login_image');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Change link and title for the Wordpress logo on the login page
+
+function add_custom_login_url(){
+    return "http://www.admium.nl/";
+}
+add_filter('login_headerurl', 'add_custom_login_url');
+
+function add_custom_login_title(){
+    return "Admium - online strategie & realisatie";
+}
+add_filter('login_headertitle', 'add_custom_login_title');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Change copyright notification at the bottom of the page
+
+function add_custom_footer_admin () {
+	echo '&copy; '.date("Y").' - Admium';
+}
+add_filter('admin_footer_text', 'add_custom_footer_admin');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Disable certain default Wordpress dashboard widgets
+
+function disable_default_dashboard_widgets() {
+	remove_meta_box('dashboard_incoming_links', 'dashboard', 'core'); // remove incoming links widget
+	remove_meta_box('dashboard_plugins', 'dashboard', 'core'); // remove plugins widget
+	remove_meta_box('dashboard_quick_press', 'dashboard', 'core'); // remove quick press widget
+	remove_meta_box('dashboard_primary', 'dashboard', 'core'); // remove wordpress blog feed widget
+	remove_meta_box('dashboard_secondary', 'dashboard', 'core'); // remove other wordpress news widget
+	remove_meta_box('rg_forms_dashboard', 'dashboard', 'normal'); // remove gravity forms widget
+	remove_meta_box('yoast_db_widget', 'dashboard', 'normal'); // remove yoast widget
+}
+add_action('admin_menu', 'disable_default_dashboard_widgets', 1);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Add iFrame to Wordpress dashboard which loads Admium documentation
+
+function admium_custom_dashboard_widgets() {
+	wp_add_dashboard_widget('custom_help_widget', 'Hulp nodig? Admium deelt kennis', 'custom_dashboard_help');
+	
+	// Globalize the metaboxes array, this holds all the widgets for wp-admin
+	global $wp_meta_boxes;
+	
+	// Get the regular dashboard widgets array 
+	// (which has our new widget already but at the end)
+	$normal_dashboard = $wp_meta_boxes['dashboard']['normal']['core'];
+	
+	// Backup and delete our new dashboard widget from the end of the array
+	$example_widget_backup = array('custom_help_widget' => $normal_dashboard['custom_help_widget']);
+	unset($normal_dashboard['custom_help_widget']);
+
+	// Merge the two arrays together so our widget is at the beginning
+	$sorted_dashboard = array_merge($example_widget_backup, $normal_dashboard);
+
+	// Save the sorted array back into the original metaboxes 
+	$wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dashboard;
+}
+function custom_dashboard_help() {
+	echo '<iframe src="http://service.admium.nl/widget/" width="100%" height="500"></iframe>';
+}
+add_action('wp_dashboard_setup', 'admium_custom_dashboard_widgets');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+require (dirname(__FILE__).'/shortcodes.php');
---
+++ shortcodes.php
@@ -0,0 +1,68 @@
+<?php
+
+function adm_sitemap($atts, $content = null, $code) {
+	if(isset($atts['type'])){
+		switch($atts['type']){
+			case 'pages':
+				return adm_sitemap_pages($atts);
+			case 'posts':
+			default:
+				return adm_sitemap_posts($atts);	
+		}
+	}
+	return '';
+}
+
+add_shortcode('sitemap', 'adm_sitemap');
+
+function adm_sitemap_pages($atts){
+	extract(shortcode_atts(array(
+		'number' => '0',
+		'depth' => '0',
+	), $atts));
+	
+	return '<div class="sc-sitemap sitemap-pages"><ul>'.wp_list_pages('depth=0&sort_column=menu_order&echo=0&title_li=&depth='.$depth.'&number='.$number ).'</ul></div>';
+}
+
+function adm_sitemap_posts($atts){
+	extract(shortcode_atts(array(
+		'comments_number' => true,
+		'number' => '0',
+		'cat' => '',
+		'posts' => '',
+		'author' => '',
+        'type' => 'post'
+	), $atts));
+	
+	if($number == 0){
+		$number = 1000;
+	}
+	if($comments_number === 'false'){
+		$comments_number = false;
+	}
+	
+	$query = array(
+		'showposts' => (int)$number,
+		'post_type'=>$type//'post',
+	);
+	if($cat){
+		$query['cat'] = $cat;
+	}
+	if($posts){
+		$query['post__in'] = explode(',',$posts);
+	}
+	if($author){
+		$query['author'] = $author;
+	}
+	$archive_query = new WP_Query( $query );
+	
+	$output = '';
+	while ($archive_query->have_posts()) : $archive_query->the_post();
+		$output .= '<li><a href="'.get_permalink().'" rel="bookmark" title="'.sprintf( __("Permanent Link to %s", 'striking_front'), get_the_title() ).'">'. get_the_title().'</a>'.($comments_number?' ('.get_comments_number().')':'').'</li>';
+	endwhile;
+	
+	wp_reset_query();
+	
+	return '<div class="sc-sitemap sitemap-posts"><ul>'.$output.'</ul></div>';
+}
+?>
